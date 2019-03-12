13cebadf6bd97e8ab6888114f2e4259ec199e808
kaspiotr <kaspiotr@gmail.com>
2019-03-12 09:54:32 +0100

    Add implementation of required functions and usage times handling for static and shared libraries

--- KasprzykPiotr/zestaw1/zad1/find.h
+++ KasprzykPiotr/zestaw1/zad1/find.h
@@ -1,6 +1,20 @@
 #ifndef PROJECT_KASPIOTR_FINDLIB_H
 #define PROJECT_KASPIOTR_FINDLIB_H
 
-void save_find_result_to_file(char *directory_path, char *file_name, char *tmp_file_name);
+typedef struct blocks_array {
+    int blocks_array_size;
+    char **blocks;
+    int *block_sizes;
+} blocks_array;
+
+void create_blocks_array(blocks_array *array, int blocks_array_size);
+
+void delete_blocks_array(blocks_array *array);
+
+int save_temp_file_to_block(blocks_array *array, char *temp_file_name);
+
+void remove_block(blocks_array *array, int block_index);
+
+void save_find_result_to_temp_file(char *directory_path, char *file_name, char *temp_file_name);
 
 #endif //PROJECT_KASPIOTR_FINDLIB_H
--- KasprzykPiotr/zestaw1/zad1/find_test.c
+++ KasprzykPiotr/zestaw1/zad1/find_test.c
@@ -1,8 +1,130 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/resource.h>
+#include <time.h>
 #include "find.h"
 
-int main(int argc, char* argv[]) {
-	save_find_result_to_file(".", "*.txt", "tempfile.txt");
-	return 0;
+void print_time(double time) {
+    int minutes = (int) (time / 60);
+    double seconds = time - minutes * 60;
+    printf("%dm %.9fs\n", minutes, seconds);
+}
+
+double count_time_range(struct timeval end_time, struct timeval start_time) {
+    double tmp_end_time = ((double) end_time.tv_sec) + (((double) end_time.tv_usec) / 1000000);
+    double tmp_start_time = ((double) start_time.tv_sec) + (((double) start_time.tv_usec) / 1000000);
+    return tmp_end_time - tmp_start_time;
+}
+
+void print_function_execution_time(char* function_name, clock_t real_start, clock_t real_end, struct timeval sys_start, struct timeval sys_end, struct timeval user_start, struct timeval user_end) {
+    printf("%s function execution time:\n", function_name);
+    printf("real:\t");
+    print_time(((double) real_end - real_start) / CLOCKS_PER_SEC);
+    printf("system:\t");
+    print_time(count_time_range(sys_end, sys_start));
+    printf("user:\t");
+    print_time(count_time_range(user_end, user_start));
+    printf("\n");
+}
+
+int main(int argc, char **argv) {
+
+    if (argc < 2) {
+        fprintf(stderr, "wrong input format\n");
+        exit(1);
+    }
+
+    blocks_array array;
+    int array_size;
+    clock_t real_start, real_end;
+    struct rusage ru_start, ru_end;
+    struct timeval sys_start, sys_end, user_start, user_end;
+
+    if (strcmp(argv[1], "create_table") == 0) {
+        array_size = atoi(argv[2]);
+
+        real_start = clock();
+        getrusage(RUSAGE_SELF, &ru_start);
+        create_blocks_array(&array, array_size);
+        real_end = clock();
+        getrusage(RUSAGE_SELF, &ru_end);
+        sys_start = ru_start.ru_stime;
+        user_start = ru_start.ru_utime;
+        sys_end = ru_end.ru_stime;
+        user_end = ru_end.ru_utime;
+        print_function_execution_time("create_table", real_start, real_end, sys_start, sys_end, user_start, user_end);
+
+    } else {
+        fprintf(stderr, "cannot perform any further operations on uninitialized table\n");
+        exit(1);
+    }
+
+    int i = 3;
+    while (i < argc) {
+        char *buffer = argv[i];
+
+        if (strcmp(argv[i], "search_directory") == 0) {
+            char* directory_path = argv[i+1];
+            char* file_name = argv[i+2];
+            char* temp_file_name = argv[i+3];
+
+            real_start = clock();
+            getrusage(RUSAGE_SELF, &ru_start);
+            save_find_result_to_temp_file(directory_path, file_name, temp_file_name);
+            real_end = clock();
+            getrusage(RUSAGE_SELF, &ru_end);
+            sys_start = ru_start.ru_stime;
+            user_start = ru_start.ru_utime;
+            sys_end = ru_end.ru_stime;
+            user_end = ru_end.ru_utime;
+            print_function_execution_time("save_find_result_to_temp_file", real_start, real_end, sys_start, sys_end, user_start, user_end);
+
+            i += 4;
+        } else if (strcmp(argv[i], "remove_block") == 0) {
+            int index = atoi(argv[i+1]);
+
+            real_start = clock();
+            getrusage(RUSAGE_SELF, &ru_start);
+            remove_block(&array, index);
+            real_end = clock();
+            getrusage(RUSAGE_SELF, &ru_end);
+            sys_start = ru_start.ru_stime;
+            user_start = ru_start.ru_utime;
+            sys_end = ru_end.ru_stime;
+            user_end = ru_end.ru_utime;
+            print_function_execution_time("remove_block", real_start, real_end, sys_start, sys_end, user_start, user_end);
+
+            i += 2;
+        } else if (strcmp(argv[i], "save_temp_file_to_block") == 0) {
+            char *temp_file_name = argv[i+1];
+
+            real_start = clock();
+            getrusage(RUSAGE_SELF, &ru_start);
+            save_temp_file_to_block(&array, temp_file_name);
+            real_end = clock();
+            getrusage(RUSAGE_SELF, &ru_end);
+            sys_start = ru_start.ru_stime;
+            user_start = ru_start.ru_utime;
+            sys_end = ru_end.ru_stime;
+            user_end = ru_end.ru_utime;
+            print_function_execution_time("save_temp_file_to_block", real_start, real_end, sys_start, sys_end, user_start, user_end);
+
+            i += 2;
+        }
+    }
+
+    real_start = clock();
+    getrusage(RUSAGE_SELF, &ru_start);
+    delete_blocks_array(&array);
+    real_end = clock();
+    getrusage(RUSAGE_SELF, &ru_end);
+    sys_start = ru_start.ru_stime;
+    user_start = ru_start.ru_utime;
+    sys_end = ru_end.ru_stime;
+    user_end = ru_end.ru_utime;
+    print_function_execution_time("delete_blocks_array", real_start, real_end, sys_start, sys_end, user_start, user_end);
+//    if (strcmp(argv[1], ""))
+//        save_find_result_to_temp_file(".", "*.txt", "tempfile.txt");
+    return 0;
 }
