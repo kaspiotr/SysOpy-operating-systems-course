091c37d4408cdcd2e9cef7410e5b42f8c3fd713a
kaspiotr <kaspiotr@gmail.com>
2019-03-21 19:39:57 +0100

    Add sort function implementation

--- KasprzykPiotr/cw02/zad1/program.c
+++ KasprzykPiotr/cw02/zad1/program.c
@@ -58,6 +58,196 @@ void generate(int argc, char **argv) {
 }
 
 /**
+ * Sorts records in a given file using insertion sort
+ * @param argc is a number of the program input arguments
+ * @param argv contains an array of pointers to input arguments
+ */
+void sort(int argc, char **argv) {
+
+    if (argc < 6) {
+        fprintf(stderr, "wrong function input argument format\n");
+        exit(-1);
+    }
+
+    char *file_name = argv[2];
+    int records_number = atoi(argv[3]);
+    int record_size = atoi(argv[4]);
+    char *variant = argv[5];
+
+    char *first_unsorted_record = malloc(record_size * sizeof(char));
+    char *swap_buffer = malloc(record_size * sizeof(char));
+
+    if (strcmp(variant, "sys") == 0) {
+        int fd = open(file_name, O_RDWR);
+
+        if (fd < 0) {
+
+            fprintf(stderr, "error while opening file: %s\n", file_name);
+            exit(-1);
+        }
+
+        int i;
+        int next_record_position;
+        int target_record_position;
+        for (i = 1; i < records_number; i++) {
+            next_record_position = i * record_size;
+            target_record_position = 0; // record position after insertion sort is complete
+
+            // setting fd to to the beginning of the next record - next_record_position
+            if (lseek(fd, next_record_position * sizeof(char), SEEK_SET) == 0) {
+                fprintf(stderr, "error while opening file: %s\n", file_name);
+                exit(-1);
+            }
+
+            // loading next record from file to first_unsorted_record array
+            if(read(fd, first_unsorted_record, record_size * sizeof(char)) != record_size * sizeof(char)) {
+                fprintf(stderr, "error while reading from file: %s\n", file_name);
+                exit(-1);
+            }
+
+            // search for position of the record, where it should be after insertion sort is completed
+            int j = i;
+            int record_position;
+            while (j > 0) {
+                record_position = record_size * j;
+
+                // finding position of previous record
+                if (lseek(fd, (record_position - record_size) * sizeof(char), SEEK_SET) < 0) {
+                    fprintf(stderr, "error while setting pointer in a file: %s\n", file_name);
+                    exit(-1);
+                }
+
+                // loading record from file into swap_buffer
+                if (read(fd, swap_buffer, record_size * sizeof(char)) != record_size * sizeof(char)) {
+                    fprintf(stderr, "error while reading from file: %s\n", file_name);
+                    exit(-1);
+                }
+
+                // loop ends when proper position is found
+                if ((unsigned char) first_unsorted_record[0] >= (unsigned char) swap_buffer[0]) { // we have found target record position
+                    target_record_position = record_position;
+                    break;
+                } else { // if record position does not point to target_record_position
+                    if(lseek(fd, record_position * sizeof(char), SEEK_SET) < 0) {
+                        fprintf(stderr, "error while setting pointer in a file: %s\n", file_name);
+                        exit(-1);
+                    }
+
+                    if(write(fd, swap_buffer, record_size * sizeof(char)) != record_size * sizeof(char)){
+                        fprintf(stderr, "error while writing to file: %s\n", file_name);
+                        exit(-1);
+                    }
+                }
+                j--;
+            }
+
+            // inserting record into target_record_position in file
+            if(lseek(fd, target_record_position * sizeof(char), SEEK_SET) < 0) {
+                fprintf(stderr, "error while setting pointer in file: %s\n", file_name);
+                exit(-1);
+            }
+
+            if(write(fd, first_unsorted_record, record_size * sizeof(char)) != record_size * sizeof(char)){
+                fprintf(stderr, "error while writing into file: %s\n", file_name);
+                exit(-1);
+            }
+        }
+
+        // closing file
+        if(close(fd) != 0) {
+            fprintf(stderr, "error while closing file: %s\n", file_name);
+            exit(-1);
+        }
+    } else if (strcmp(variant, "lib") == 0) {
+
+        FILE *file = fopen(file_name, "r+");
+
+        if (file == NULL) {
+            fprintf(stderr, "error while opening file: %s\n", file_name);
+            exit(-1);
+        }
+
+        int i;
+        int next_record_position;
+        int target_record_position;
+        for(i=0; i < records_number; i++) {
+            next_record_position = i * record_size;
+            target_record_position = 0;
+
+            if(fseek(file, next_record_position * sizeof(char), SEEK_SET) != 0) {
+                fprintf(stderr, "error while setting file pointer in file: %s\n", file_name);
+                exit(-1);
+            }
+
+            // loading next record from file to first_unsorted_record array
+            if(fread(first_unsorted_record, sizeof(char), record_size, file) != record_size) {
+                fprintf(stderr, "error while reading from file: %s\n", file_name);
+                exit(-1);
+            }
+
+            // search for position of the record, where it should be after insertion sort is completed
+            int j = i;
+            int record_position;
+            while (j > 0) {
+                record_position = record_size * j;
+
+                // finding position of previous record
+                if (fseek(file, (record_position - record_size) * sizeof(char), SEEK_SET) != 0) {
+                    fprintf(stderr, "error while setting pointer in a file: %s\n", file_name);
+                    exit(-1);
+                }
+
+                // loading record from file into swap_buffer
+                if (fread(swap_buffer, sizeof(char), record_size, file) != record_size) {
+                    fprintf(stderr, "error while reading from file: %s\n", file_name);
+                    exit(-1);
+                }
+
+                // loop ends when proper position is found
+                if ((unsigned char) first_unsorted_record[0] >= (unsigned char) swap_buffer[0]) { // we have found target record position
+                    target_record_position = record_position;
+                    break;
+                } else { // if record position does not point to target_record_position
+                    if(fseek(file, record_position * sizeof(char), SEEK_SET) != 0) {
+                        fprintf(stderr, "error while setting pointer in a file: %s\n", file_name);
+                        exit(-1);
+                    }
+
+                    if(fwrite(swap_buffer, sizeof(char), record_size, file) != record_size){
+                        fprintf(stderr, "error while writing to file: %s\n", file_name);
+                        exit(-1);
+                    }
+                }
+
+                j--;
+            }
+
+            // inserting record into target_record_position in file
+            if(fseek(file, target_record_position * sizeof(char), SEEK_SET) != 0) {
+                fprintf(stderr, "error while setting pointer in file: %s\n", file_name);
+                exit(-1);
+            }
+
+            if(fwrite(first_unsorted_record, sizeof(char), record_size, file) != record_size){
+                fprintf(stderr, "error while writing into file: %s\n", file_name);
+                exit(-1);
+            }
+        }
+
+        // closing file
+        if(fclose(file) != 0) {
+
+            fprintf(stderr, "error while closing file: %s\n", file_name);
+            exit(-1);
+        }
+
+    } else {
+        fprintf(stderr, "wrong function input argument format\n");
+        exit(-1);
+    }
+}
+
+/**
  * Copies one file to another
  * @param argc is a number of the program input arguments
  * @param argv contains an array of pointers to input arguments
@@ -161,17 +351,13 @@ int main(int argc, char **argv) {
 
     if (strcmp(argv[1], "generate") == 0) {
         generate(argc, argv);
-    } else if(strcmp(argv[1], "sort") == 0) {
+    } else if (strcmp(argv[1], "sort") == 0) {
+        sort(argc, argv);
 
-
-    }
-
-    else if(strcmp(argv[1], "copy") == 0) {
+    } else if (strcmp(argv[1], "copy") == 0) {
 
         copy(argc, argv);
-    }
-
-    else {
+    } else {
 
         fprintf(stderr, "unknown command: %s\n", argv[1]);
         exit(-1);
